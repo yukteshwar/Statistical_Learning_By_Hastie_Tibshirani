748ff163fa69761af2cd6a9d3ad04d9dd96ccb8d
yukteshwar <byukteshwar@gmail.com>
2018-08-14 23:45:09 -0400

    Chapter 3: Problem 15

--- Chapter_03/Problem_15.R
+++ Chapter_03/Problem_15.R
@@ -25,27 +25,53 @@ library(MASS)
 # the response. Describe your results. In which of the models is
 # there a statistically significant association between the predictor
 # and the response? Create some plots to back up your assertions.
-lm.fit = lm(crim~zn)
+lm.fit = lm(crim~zn, data = Boston)
 plot(zn, crim)
 abline(lm.fit, lwd = 3, col = "red")
 
-lm.fit = lm(crim~indus)
-plot(indus, crim)
-abline(lm.fit, lwd = 3, col = "red")
+# lm.fit = lm(crim~indus)
+# plot(indus, crim)
+# abline(lm.fit, lwd = 3, col = "red")
+# 
+# lm.fit = lm(crim~chas)
+# plot(chas, crim)
+# abline(lm.fit, lwd = 3, col = "red")
+# 
+# lm.fit = lm(crim~nox)
+# plot(nox, crim)
+# abline(lm.fit, lwd = 3, col = "red")
+# 
+# lm.fit = lm(crim~rm)
+# plot(rm, crim)
+# abline(lm.fit, lwd = 3, col = "red")
+# 
+# lm.fit = lm(crim~age)
+# plot(age, crim)
+# abline(lm.fit, lwd = 3, col = "red")
 
-lm.fit = lm(crim~chas)
-plot(chas, crim)
-abline(lm.fit, lwd = 3, col = "red")
+# Skipping rest of them
 
-lm.fit = lm(crim~nox)
-plot(nox, crim)
-abline(lm.fit, lwd = 3, col = "red")
+# Fit a multiple regression model to predict the response using
+# all of the predictors. Describe your results. For which predictors
+# can we reject the null hypothesis H0: beta_j = 0
+lm.fitall = lm(crim~., data = Boston)
+print(summary(lm.fitall))
+print("Based on p-values, we reject the null hypothesis H0: beta_j = 0 for all variables except dis, rad & medv")
 
-lm.fit = lm(crim~rm)
-plot(rm, crim)
-abline(lm.fit, lwd = 3, col = "red")
+# How do your results from (a) compare to your results from (b)?
+# Create a plot displaying the univariate regression coefficients
+# from (a) on the x-axis, and the multiple regression coefficients
+# from (b) on the y-axis
+individual.coeffs = list()
+#nPred = length(sapply(Boston, class)) - 1
+predictors = colnames(Boston)
+nPred = length(predictors)
+for(i in 2:length(predictors))
+{
+  lm.fit = lm(crim~Boston[,i], data = Boston)
+  individual.coeffs = c(individual.coeffs, lm.fit$coefficients[2])
+}
 
-lm.fit = lm(crim~age)
-plot(age, crim)
-abline(lm.fit, lwd = 3, col = "red")
+
+plot(individual.coeffs, lm.fitall$coefficients[2:nPred])
         
